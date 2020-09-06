6ba569064ddb302999e48b9dfdd703e9fefe1e4f
chapeau <mou.idri@gmail.com>
2020-09-06 23:11:40 +0200

    Add files via upload

---
+++ PROD/SpokeNetworkV1/main.tf
@@ -0,0 +1,95 @@
+data "azurerm_client_config" "current" {}
+output "current_client_id" {
+  value = data.azurerm_client_config.current.client_id
+}
+output "current_tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}
+output "current_subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
+output "current_object_id" {
+  value = data.azurerm_client_config.current.object_id
+}
+
+module "rg" {
+  source               = "./modules/rg"
+  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
+  preferred-location-module = "${var.preferred-location-main}"
+  tags-rg-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="network"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"
+  }
+}
+
+#CI Validated so far 
+module "logging" {
+  source               = "./modules/logging"
+  current-vm-default-pass-module = "${var.current-vm-default-pass-main}"
+  current-vm-default-username-module = "${var.current-vm-default-username-main}"
+  current-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
+  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
+  preferred-location-module = "${var.preferred-location-main}"
+  current-az-sp-object-id-module = data.azurerm_client_config.current.object_id
+  current-az-sp-tenant-id-module = data.azurerm_client_config.current.tenant_id
+  tags-sto-logging-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="storage"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"}
+  stoc_depend_on_module = [module.rg ]
+  tags-repo-logging-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="logging"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"}
+  logacc_depend_on_module = [module.rg]
+}
+#CI Validated so far 
+module "network" {
+  source               = "./modules/network"
+  current-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
+  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
+  preferred-location-module = "${var.preferred-location-main}"
+  tags-vnet-module = {
+    environment = "production"
+    scope_1="shared_infrastructure"
+    scope_2="core_infrastructure"
+    type_1="network_security"
+    type_2="network"
+    lob="it_infrastructure"
+    business_location="corpc"
+    projectowner="it_transverse_cloud_team"}
+  ip-range-module = "${var.current-vnet-space}"
+  vnet_depend_on_module = [module.rg]
+}
+
+# ROUTING & ADMIN PART :
+module "subnet-nsg-route-spoke-storage" {
+  current-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
+  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
+  preferred-location-module = "${var.preferred-location-main}"  
+  source               = "./modules/generic-subnet-nsg"
+  #root-name-subnet-module = "privatedmzin"
+  root-name-subnet-module = "${var.spoke-storage-root-name}"  
+  #iprange-subnet-module = "10.255.254.32/28"
+  iprange-subnet-module = "${var.spoke-storage-subnet-range}" 
+  #portrange-subnet-module =  ["21-4950"]
+  portrange-subnet-module =  ["${var.spoke-storage-nsg-port-range}"]
+  nva-ip-out-to-spoke-module = "${var.nva-ip-out-to-spoke-main}" 
+  subnet_depend_on_module = [module.network,module.rg]
+}
---
+++ PROD/SpokeNetworkV1/modules/generic-subnet-nsg/main.tf
@@ -0,0 +1,61 @@
+#create subnet 
+resource "azurerm_subnet" "subnet-iac" {
+  name                = "${var.current-name-convention-core-module}-subnet-${var.root-name-subnet-module}"
+  resource_group_name  = "${var.current-name-convention-core-module}-rg"
+  virtual_network_name = "${var.current-name-convention-core-module}-vnet"
+  address_prefix       = "${var.iprange-subnet-module}"
+  depends_on = [var.subnet_depend_on_module]
+  
+} 
+
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg-iac" {
+    depends_on = [var.subnet_depend_on_module]
+    name                = "${var.current-name-convention-core-module}-subnet-${var.root-name-subnet-module}-nsg"
+    location            = "${var.preferred-location-module}" 
+    resource_group_name = "${var.current-name-convention-core-module}-rg"
+    security_rule {
+        name                       = "nsgruleallowdefault"
+        priority                   = 4001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_ranges     = "${var.portrange-subnet-module}" 
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+}
+resource "azurerm_subnet_network_security_group_association" "subnet-nsg-assoc-iac" {
+  subnet_id                 = azurerm_subnet.subnet-iac.id
+  network_security_group_id = azurerm_network_security_group.nsg-iac.id
+}
+
+
+resource "azurerm_route_table" "route-nva-iac" {
+  depends_on = [var.subnet_depend_on_module]
+  name                          = "route-table-${var.root-name-subnet-module}"
+  location                      = "${var.preferred-location-module}" 
+  resource_group_name           = "${var.current-name-convention-core-module}-rg"
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route-table-${var.root-name-subnet-module}-to-nva-default"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "${var.nva-ip-out-to-spoke-module}"
+  }
+}
+resource "azurerm_subnet_route_table_association" "route-nva-subnet-asso-iac" {
+  subnet_id      = azurerm_subnet.subnet-iac.id
+  route_table_id = azurerm_route_table.route-nva-iac.id
+}
+
+output "durty-temp-outupt0" {
+  value = "${var.preferred-location-module}" 
+}
+output "durty-temp-outupt1" {
+  value ="${var.current-name-convention-core-public-module}"  
+}
