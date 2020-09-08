7a74c531d340a300e703ef1c77ba0c0f8f851c21
mou.idri@gmail.com <mou.idri@gmail.com>
2020-09-08 09:55:44 +0200

    storage change test

--- PREPROD/SpokeStorageNFSV1/main.tf
+++ PREPROD/SpokeStorageNFSV1/main.tf
@@ -19,12 +19,6 @@ resource "azurerm_resource_group" "resource_group_spoke" {
 }
 
 
-data "azurerm_subnet" "subnet-spoke-storage" {
-  name                = "${var.current-name-convention-core-network-main}-subnet-${var.spoke-storage-root-name}"
-  virtual_network_name = "${var.current-name-convention-core-network-main}-vnet"
-  resource_group_name = "${var.current-name-convention-core-network-main}-rg"
-}
-
 #CI Validated so far 
 module "logging" {
   source               = "./modules/logging"
@@ -39,22 +33,28 @@ module "logging" {
   logacc_depend_on_module = [azurerm_resource_group.resource_group_spoke]
 }
 
-#CI Validated so far test
-#CI Validated so far test
-module "vm-nfs-sto-1" {
-  current-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
-  current-name-convention-core-module  = "${var.current-name-convention-core-main}"
-  preferred-location-module = "${var.preferred-location-main}"  
-  source               = "./modules/vm-lnx-pri"
-  subnet_in_id_module = data.azurerm_subnet.subnet-spoke-storage.id
-  ip-in-mtl-module = "${var.vm-nfs-sto-1-private-ip-address}" 
-  mtl-size ="${var.vmsize_small_1_2}"
-  mtl-login = "${var.current-vm-default-username-main}"
-  mtl-passwd = "${var.current-vm-default-pass-main}"
-  stor-log-repo = "${module.logging.hub-corpc-sto-acc-log-endpoint}"
-  stor-log-repo-name = "${module.logging.hub-corpc-sto-acc-log-name}"
-  stor-log-repo-sas = "${module.logging.hub-corpc-sto-acc-log-sas-url-string}"
-  stor-log-ws-crd-1 = "${module.logging.hub-corpc-log-ana-rep-primary-workspace-id}"
-  stor-log-ws-crd-2 = "${module.logging.hub-corpc-log-ana-rep-primary-key}"  
-  mtl_depend_on = [azurerm_resource_group.resource_group_spoke, module.logging ]
-}
+#dat#a "azurerm_subnet" "subnet-spoke-storage" {
+#  n#ame                = "${var.current-name-convention-core-network-main}-subnet-${var.spoke-storage-root-name}"
+#  v#irtual_network_name = "${var.current-name-convention-core-network-main}-vnet"
+#  resource_group_name = "${var.current-name-convention-core-network-main}-rg"
+#}
+
+#CI #Validated so far test
+#CI #Validated so far test
+#mod#ule "vm-nfs-sto-1" {
+#  c#urrent-name-convention-core-public-module = "${var.current-name-convention-core-public-main}"
+#  c#urrent-name-convention-core-module  = "${var.current-name-convention-core-main}"
+#  p#referred-location-module = "${var.preferred-location-main}"  
+#  s#ource               = "./modules/vm-lnx-pri"
+#  s#ubnet_in_id_module = data.azurerm_subnet.subnet-spoke-storage.id
+#  i#p-in-mtl-module = "${var.vm-nfs-sto-1-private-ip-address}" 
+#  m#tl-size ="${var.vmsize_small_1_2}"
+#  m#tl-login = "${var.current-vm-default-username-main}"
+#  m#tl-passwd = "${var.current-vm-default-pass-main}"
+#  s#tor-log-repo = "${module.logging.hub-corpc-sto-acc-log-endpoint}"
+#  s#tor-log-repo-name = "${module.logging.hub-corpc-sto-acc-log-name}"
+#  s#tor-log-repo-sas = "${module.logging.hub-corpc-sto-acc-log-sas-url-string}"
+#  s#tor-log-ws-crd-1 = "${module.logging.hub-corpc-log-ana-rep-primary-workspace-id}"
+#  s#tor-log-ws-crd-2 = "${module.logging.hub-corpc-log-ana-rep-primary-key}"  
+#  mtl_depend_on = [azurerm_resource_group.resource_group_spoke, module.logging ]
+#}
