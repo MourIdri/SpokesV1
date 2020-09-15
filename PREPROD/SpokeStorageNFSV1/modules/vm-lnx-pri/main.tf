bba735931ab84aee640cbe326c9d676717db2628
root <root@DESKTOP-423ITP5.localdomain>
2020-09-15 21:40:21 +0200

    spoke storage check

--- PREPROD/SpokeStorageNFSV1/modules/vm-lnx-pri/main.tf
+++ PREPROD/SpokeStorageNFSV1/modules/vm-lnx-pri/main.tf
@@ -103,7 +103,6 @@ resource "azurerm_virtual_machine_extension" "DAAgentForLinux" {
   type_handler_version       = "9.5"
   auto_upgrade_minor_version = true
   depends_on                 = [azurerm_virtual_machine_extension.mtl_oms_mma]
-
 }
 
 resource "azurerm_virtual_machine_extension" "mtl_diag_setting" {
