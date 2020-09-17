746370b0375fd6def3c98b7bfcad100c51761d49
root <root@DESKTOP-423ITP5.localdomain>
2020-09-17 13:37:31 +0200

    spoke storage logging changes

--- PREPROD/SpokeStorageS3V1/output.tf
+++ PREPROD/SpokeStorageS3V1/output.tf
@@ -1,3 +1,23 @@
 output "subscription_id" {
   value = "${data.azurerm_client_config.current.subscription_id}"
 }
+output "logging_hub_loganalytics_stor_log_repo_out" {
+  value = "${module.logging.hub-corpc-sto-acc-log-endpoint}"
+}
+
+output "logging_hub_loganalytics_stor_log_repo_name_out" {
+  value = "${module.logging.hub-corpc-sto-acc-log-name}"
+}
+
+output "logging_hub_loganalytics_stor_log_repo_sas_out" {
+  value = "${module.logging.hub-corpc-sto-acc-log-sas-url-string}"
+}
+
+output "logging_hub_loganalytics_ws_id_out" {
+  value = "${module.logging.hub-corpc-log-ana-rep-primary-workspace-id}"
+}
+
+output "logging_hub_loganalytics_ws_key_out" {
+  value = "${module.logging.hub-corpc-log-ana-rep-primary-key}"
+}
+
